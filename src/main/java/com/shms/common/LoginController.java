4e7eb9073bb9c3c1e9c43c91408ea69dc31e9e62
H <H@DESKTOP-N1VKAAO>
2021-05-25 20:48:24 +0900

    login

--- src/main/java/com/shms/common/LoginController.java
+++ src/main/java/com/shms/common/LoginController.java
@@ -2,17 +2,24 @@ package com.shms.common;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shms.manager.Manager;
 
 @RestController
+@RequestMapping("/common")
 public class LoginController {
+	
+	@Autowired
+	LoginServiceImpl loginService;
 	
 	@GetMapping("/login/form")
 	public ModelAndView loginForm() {
@@ -20,8 +27,10 @@ public class LoginController {
 	}
 	
 	@PostMapping("/login")
-	public ModelAndView login(@ModelAttribute Manager manager) {
+	public ModelAndView login(@ModelAttribute Manager manager, HttpSession httpSession) {
 		ModelAndView modelAndView = new ModelAndView();
+		
+		modelAndView.setView(new RedirectView(loginService.login(manager, httpSession)));
 		
 		return modelAndView;
 	}
@@ -29,5 +38,23 @@ public class LoginController {
 	@GetMapping("/logout")
 	public ModelAndView logout(HttpSession httpSession) {
 		return new ModelAndView(new RedirectView("/login/form"));
+	}
+	
+	@GetMapping("/check/{empNumber}/{password}")
+	public String checkLogin(Manager manager) {
+		ObjectMapper mapper = new ObjectMapper();
+		String result = "{}";
+		
+		try {
+			if (loginService.loginCheck(manager) != null) {
+				result = mapper.writeValueAsString(manager);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException();
+		}
+		
+		System.out.println("test : " + result);
+		return result;
 	}
 }
