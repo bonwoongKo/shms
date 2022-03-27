b1d52fc4ee2d753fc089cb885de7db778b2b70b5
bonwoongKo <bonjourkbw@gmail.com>
2022-03-27 18:46:50 +0900

    2022-03-27 18:45 bonwoong -deploy.sh 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,4 +33,6 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar \ -Dspring.config.location=classpath:/application.properties \ -Dspring.profiles.active=real \ $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar \
+  -Dspring.config.location=classpath:/application.properties \
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
