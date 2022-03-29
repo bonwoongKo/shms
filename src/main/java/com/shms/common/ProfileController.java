04446c38d39dfa35b5ea2e281dd603c93980721f
bonwoongKo <bonjourkbw@gmail.com>
2022-03-29 23:27:14 +0900

    2022-03-29 23:24 bonwoong -ProfileController 추가 및 application 파일 수정

---
+++ src/main/java/com/shms/common/ProfileController.java
@@ -0,0 +1,28 @@
+package com.shms.common;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProfileController {
+	private final Environment env;
+	
+	@Autowired
+	public ProfileController(Environment env) {
+		this.env = env;
+	}
+	
+	@GetMapping("/profile")
+	public String profile() {
+		List<String> profiles = Arrays.asList(env.getActiveProfiles());
+		List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+		String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+		
+		return profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfile);
+	}
+}
