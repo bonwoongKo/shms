f13adde5ad98230a1f6cf1c36a3d294eff231420
Srltas <kdm97912@gmail.com>
2022-04-07 17:01:42 +0900

    test: travis ci 배포 테스트

--- src/main/java/com/shms/ShmsApplication.java
+++ src/main/java/com/shms/ShmsApplication.java
@@ -8,6 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan(basePackages = {"com.shms"})
 public class ShmsApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ShmsApplication.class, args);
+		SpringApplication.run(ShmsApplication.class, args); 
 	}
 }
