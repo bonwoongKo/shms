68c4ff12fce1234dc191ac0bcae53ecb2012cc2c
bonwoongKo <bonjourkbw@gmail.com>
2022-04-11 17:36:01 +0900

    * fix: server 구동 에러 해결 위한 어노테이션 추가

--- src/main/java/com/shms/equipment/service/impl/EquipmentServiceImpl.java
+++ src/main/java/com/shms/equipment/service/impl/EquipmentServiceImpl.java
@@ -3,11 +3,13 @@ package com.shms.equipment.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.shms.equipment.service.Equipment;
 import com.shms.equipment.service.EquipmentMapper;
 import com.shms.equipment.service.EquipmentService;
 
+@Service
 public class EquipmentServiceImpl implements EquipmentService{
 	@Autowired
 	private EquipmentMapper equipmentMapper;
